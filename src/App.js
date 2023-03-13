8ce3711aea0f20e26c0eda6f12e3b9f3345b5ba4
Marijana <makizaric52@gmail.com>
2023-03-13 21:47:51 +0100

    Uradjena komponenta Onama i Footer i css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import NavBar from './components/NavBar';
 import './App.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Home from './components/Home';
+import ONama from './components/ONama';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
         <NavBar></NavBar>
         <Routes>
             <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/onama" element={<ONama></ONama>}></Route>
           </Routes>
      </BrowserRouter>
+     <Footer></Footer>
   </div>
   );
 }
