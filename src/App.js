4f08f76afcfa18b36654f4d6452f4343d6114d5c
Marijana <makizaric52@gmail.com>
2023-03-13 21:38:37 +0100

    Uradjena Home komponenta i njen css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import NavBar from './components/NavBar';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
 
-
     <div>
     <BrowserRouter>
         <NavBar></NavBar>
+        <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+          </Routes>
      </BrowserRouter>
   </div>
   );
